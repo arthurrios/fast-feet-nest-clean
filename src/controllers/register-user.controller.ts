88609eb905ec0778f2aa0e0cbbc655bc474f405f
arthur <arthur.rios007@gmail.com>
2025-02-04 22:12:24 -0300

    chore: add http client

--- src/controllers/register-user.controller.ts
+++ src/controllers/register-user.controller.ts
@@ -41,6 +41,16 @@ export class RegisterUserController {
       return new ConflictException('User with same CPF already exists')
     }
 
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      return new ConflictException('User with same email already exists')
+    }
+
     const hashedPassword = await hash(password, 8)
 
     await this.prisma.user.create({
