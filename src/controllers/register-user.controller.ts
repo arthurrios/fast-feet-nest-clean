8bb6b85d4126363c7049f21116d815aecccfae55
arthur <arthur.rios007@gmail.com>
2025-02-04 22:05:26 -0300

    chore: add zod validation pipe

---
+++ src/controllers/register-user.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcrypt'
+import { z } from 'zod'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+const registerUserBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  cpf: z.string().length(11),
+  password: z.string().min(6),
+  role: z.enum(['COURIER', 'ADMIN', 'RECIPIENT']),
+})
+
+type RegisterUserBodySchema = z.infer<typeof registerUserBodySchema>
+
+@Controller('/users')
+export class RegisterUserController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(registerUserBodySchema))
+  async handle(@Body() body: RegisterUserBodySchema) {
+    const { email, name, cpf, password, role } = body
+
+    const userWithSameCpf = await this.prisma.user.findUnique({
+      where: {
+        cpf,
+      },
+    })
+
+    if (userWithSameCpf) {
+      return new ConflictException('User with same CPF already exists')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        cpf,
+        password: hashedPassword,
+        role,
+      },
+    })
+  }
+}
